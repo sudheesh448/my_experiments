63937589ff83f59d83d1af04d8a0eed58e02d2e5
Sudheesh <sudheesh448@gmail.com>
2026-01-15 08:27:06 +0000

    quick sort

---
+++ data_structure/array/quick_sort.py
@@ -0,0 +1,21 @@
+my_array = [64,34,25,5,22,11,90,12]
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[-1]
+
+    left = []
+    right = []
+    
+    for i in range (len(arr)-1):
+        if arr[i]< pivot:
+            left.append(arr[i])
+        else:
+            right.append(arr[i])
+    print("left", left,"right",right)
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+print(quick_sort(my_array))
